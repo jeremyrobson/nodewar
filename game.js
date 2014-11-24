4fa19ea21c0593e3b783313ba529eb591220df0f
Jeremy Robson <jeremy.robson@gmail.com>
2014-11-24 02:14:43 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,38 +3,62 @@ var parties = [];
 var units = [];
 var items = [];
 
-var Item = function(unitid) {
-    this.unitid = unitid;
-    this.name = Math.random();
+var Item = function(doc) {
+    this.name = doc.name;
+    this.unitid = doc.unitid;
     items.push(this);
 };
 
-var Unit = function(userid, partyid) {
-    this.name = Math.random();
+Item.prototype.to_json = function() {
+    return {
+        name: this.name,
+        unitid: this.unitid
+    };
+};
+
+var Unit = function(doc) {
+    this.name = doc.name;
+    this._id = doc._id;
+    this.userid = doc.userid;
+    this.partyid = doc.partyid;
     this.equip = [];
     for (var i=0;i<6;i++) {
-        var newitem = new Item();
-        this.equip.push(newitem._id);
+        var newitem = new Item({"name":"DEFAULT ITEM","unitid":"blah"});
+        this.equip.push(newitem); //todo
     }
-    units.push({
-        "userid": userid,
-        "partyid": partyid,
-        "equip": this.equip
+    units.push(this);
+};
+
+Unit.prototype.to_json = function() {
+    return {
+        name: this.name,
+        userid: this.userid,
+        partyid: this.partyid
+    };
+};
+
+var get_unit_template = function(user) {
+    return {
+        "name": "DEFAULT UNIT",
+        "userid": user._id,
+        "partyid": user.party._id
+    };
+};
+
+var create_unit = function(db, user, callback) {
+    console.log("-----------------CREATING UNIT----------------------");
+    console.log(user);
+    db.add_data("unitcollection", get_unit_template(user), function(docs) {
+        var newunit = new Unit(docs[0]);
+        callback(newunit);
     });
 };
 
-var add_unit = function(userid) {
-    
-};
-
-var Party = function(data) {
-    this.id = data._id;
+var Party = function(data, unitlist) {
+    this._id = data._id;
     this.userid = data.userid;
-    this.units = [];
-    parties.push({
-        "userid": data.userid,
-        "units": this.units
-    });
+    this.unitlist = unitlist;
+    parties.push(this);
 };
 
 /*
@@ -45,15 +69,22 @@ Party.prototype.to_json = function() {
 };
 */
 
-var load_units = function(db, party, callback) {
-    db.find("unitcollection", {"partyid": this._id}, function(docs) {
-        party.units = docs; //fix
-        callback();
+var load_units = function(db, userid, partyid, callback) {
+    db.find("unitcollection", {"partyid": partyid}, function(docs) {
+        var unitlist = docs.map(function(doc) { return new Unit(doc, userid, partyid); });
+        callback(unitlist);
     });
 };
 
+var get_party_template = function(user) {
+    return {
+        "userid": user._id,
+        "gp": 1000
+    };
+};
+
 var create_party = function(db, userid, callback) {
-    db.add_data("partycollection", {"userid": userid}, function(docs) {
+    db.add_data("partycollection", get_party_template(userid), function(docs) {
         var newparty = new Party(docs[0]);
         parties.push(newparty);
         callback(newparty);
@@ -65,28 +96,35 @@ var load_party = function(db, userid, callback) {
         if (docs.length == 0) { //party not found
             console.log("***CREATING NEW PARTY BECAUSE userid WAS NOT FOUND IN partycollection***");
             create_party(db, userid, function(newparty) {
-                callback(newparty);
+                var party = new Party(docs[0], []);
+                callback(party);
             });
         }
         else {
-            var party = new Party(docs[0]); //todo: choose from multiple parties?
-            load_units(db, party, function() {
+            var party = new Party(docs[0]);
+            load_units(db, userid, docs[0]._id, function(unitlist) {
+                var party = new Party(docs[0], unitlist);
                 callback(party);
             });
         }
     });
 };
 
-var create_user = function(db, username, password, callback) {
-    db.add_data("usercollection", {"username":username, "password":password}, function() {
-        callback();
-    });
+var User = function(doc, party) {
+    this.username = doc.username;
+    this._id = doc._id;
+    this.sessionid = "";
+    this.party = party;
+    users.push(this);
+    console.log("-----------------CURRENT USERS----------------------");
+    console.log(users);
 };
 
-var add_user = function(username, sessionid, partyid) {
-    users.push({"username":username, "sessionid": sessionid});
-    console.log("-------------------CURRENT USERS------------------------");
-    console.log(users);
+var create_user = function(db, username, password, callback) {
+    db.add_data("usercollection", {"username":username, "password":password}, function(docs) {
+        var newuser = new User(docs[0]);
+        callback(newuser);
+    });
 };
 
 var find_user = function(username) {
@@ -94,7 +132,7 @@ var find_user = function(username) {
 };
 
 var remove_user = function(username) {
-    console.log("--------------------REMOVING USER-----------------------");
+    console.log("-------------------REMOVING USER---------------------");
     var index = users.map(function(a) { return a.username; }).indexOf(username);
     if (index >= 0) users.splice(index, 1);
     console.log(users);
@@ -104,14 +142,30 @@ var get_user = function(username) {
     return users.filter(function(a) { return a.username == username; })[0];
 };
 
+var validate_user = function(db, username, password, success, failure) {
+    db.find("usercollection", {"username": username}, function(docs) {
+        if (docs[0] && docs[0].password == password) {
+            load_party(db, docs[0]._id, function(party) {
+                var user = new User(docs[0], party);
+                success(user);   
+            });
+        }
+        else if (docs[0] && docs[0].password != password)
+            failure(401);
+        else
+            failure(400);
+    });
+};
+
 module.exports = {
+    create_unit: create_unit,
     create_party: create_party,
     load_party: load_party,
     create_user: create_user,
-    add_user: add_user,
     find_user: find_user,
     remove_user: remove_user,
     get_user: get_user,
     //get_party: get_party,
+    validate_user: validate_user,
     users: users
 };
