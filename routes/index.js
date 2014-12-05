e993a9e6175b179876d6904c182dc30bbb9afd1c
Jeremy Robson <jeremy.robson@gmail.com>
2014-12-05 07:17:16 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -46,138 +46,100 @@ exports.init = function(app, express) {
     });
     
     app.get("/client", function(req, res) {
-        var query = req.query;
-        var ajaxdata = (query.jsonData) ? JSON.parse(query.jsonData) : 0;
-        var success;
-        
-        if (ajaxdata && ajaxdata.header == "ping") {
-            success = function(user) {
-                user.lastping = user.duration;
-                var users = app.game.get_users();
-                var usernames = users.map(function(a) {return a.username; });
-                var battles = app.game.get_battles();
-                var battlenames = battles.map(function(a) {return a.name; });
-                var messagetext = app.game.pop_messages(user);
-                res.send({"header":"update", "data":{"usernames": usernames, "battlenames":battlenames, "text":messagetext}});
-            };
-        }
-        else if (ajaxdata && ajaxdata.header == "createbattle") {
-            success = function(user) {
-                app.game.create_battle(user, function(battle) {
-                    res.send({"header":"message", "data":battle});
-                });
-            };
-        }
-        else if (ajaxdata && ajaxdata.header == "chattext") {
-            success = function(user) {
-                console.log("GOT MESSAGE", ajaxdata.data);
-                var users = app.game.get_users();
-                app.game.push_message(users, user.username, ajaxdata.data);
-                res.send({"header":"chattext", "data":"message sent"}); //finish request
-            };
-        }
-        else
-            success = function(user) {
-                app.game.push_message([user], "SYSTEM", "Welcome to nodewar1 chat.");
-                var users = app.game.get_users();
-                app.game.push_message(users, "SYSTEM", user.username + " entered the chat.");
-                res.render("client", {userdata:JSON.stringify(user)});
-            };
-        
-        success(req.user);
+        app.game.push_message([req.user], "SYSTEM", "Welcome to nodewar1 chat.");
+        var users = app.game.get_users();
+        app.game.push_message(users, "SYSTEM", req.user.username + " entered the chat.");
+        res.render("client", {userdata:JSON.stringify(req.user)});
     });
+    
+    function get_data(username, unitindex, itemindex) {
+        var user = app.game.get_user(username);
+        var unit = (user && user.party.unitlist[unitindex]) ? user.party.unitlist[unitindex] : null;
+        var item = (unit && unit.equip[itemindex]) ? unit.equip[itemindex] : null;
+        
+        return {
+            userdata: JSON.stringify(user),
+            unitdata: JSON.stringify(unit),
+            itemdata: JSON.stringify(item),
+            unitindex: unitindex || -1,
+            itemindex: itemindex || -1
+        };
+    }
     
     app.get(["/user/:user", "/user/:user/unit/:unit", "/user/:user/unit/:unit/item/:item"], function(req, res) {
         console.log(req.params, req.query);
-        var user = app.game.get_user(req.params.user);
-        console.log(req.params.user, parseInt(req.params.unit), parseInt(req.params.item));
-        var unit = (user && typeof parseInt(req.params.unit) == "number") ? user.party.unitlist[req.params.unit] : null;
-        var item = (unit && typeof parseInt(req.params.item) == "number") ? unit.equip[req.params.item] : null;
         
-        if (req.params.item)
-            res.send(JSON.stringify(item));
+        var data = get_data(req.params.user, req.params.unit, req.params.itemindex);
+        
+        if (req.params.itemindex) //better way to do this?
+            res.render("equip", data);
         else if (req.params.unit)
-            res.send(JSON.stringify(unit));
+            res.render("unit", data);
         else if (req.params.user)
-            res.send(JSON.stringify(user));
+            res.render("user", data);
+        else
+            res.send("There was an error parsing the url.");
     });
     
-    app.get("/user", function(req, res) {
-        console.log("PARAMETERS!!!! ", req.params);
-        console.log("WHATTTT PART 2!!!", req.what);
+    app.get(["/ajax"], function(req, res) {
         var query = req.query;
-        var ajaxdata = (query.jsonData) ? JSON.parse(query.jsonData) : 0;
-        var success;
-
-        if (ajaxdata && ajaxdata.header == "addunit") {
-            success = function(user) {
-                app.game.create_unit(user, function(newunit) {
-                    if (newunit) res.send({"header":"newunit", "data":newunit});
-                    else res.send({"header":"error", "data":"Cannot add more units."});
-                });
-            };
+        var ajaxdata = (query.jsonData) ? JSON.parse(query.jsonData) : 0; //todo: move to middleware 
+        
+        //chat
+        if (ajaxdata && ajaxdata.header == "ping") {
+            req.user.lastping = req.user.duration;
+            var users = app.game.get_users();
+            var usernames = users.map(function(a) {return a.username; });
+            var battles = app.game.get_battles();
+            var battlenames = battles.map(function(a) {return a.name; });
+            var messagetext = app.game.pop_messages(req.user);
+            res.send({"header":"update", "data":{"usernames": usernames, "battlenames":battlenames, "text":messagetext}});
         }
-        else if (ajaxdata && ajaxdata.header == "getparty") {
-            success = function(user) {
-                app.game.get_party(user, function(party) {
-                    res.send({"header":"party", "data":party});
-                });
-            };
+        else if (ajaxdata && ajaxdata.header == "createbattle") {
+            app.game.create_battle(user, function(battle) {
+                res.send({"header":"message", "data":battle});
+            });
+        }
+        else if (ajaxdata && ajaxdata.header == "chattext") {
+            console.log("GOT MESSAGE", ajaxdata.data);
+            var users = app.game.get_users();
+            app.game.push_message(users, req.user.username, ajaxdata.data);
+            res.send({"header":"chattext", "data":"message sent"}); //finish request
+        }
+    
+        //units
+        else if (ajaxdata && ajaxdata.header == "addunit") {
+            app.game.create_unit(req.user, function(newunit) {
+                if (newunit) res.send({"header":"newunit", "data":newunit});
+                else res.send({"header":"error", "data":"Cannot add more units."});
+            });
+        }
+        else if (ajaxdata && ajaxdata.header == "getdata") {
+            var data = get_data(req.user.username, ajaxdata.data.unitindex, ajaxdata.data.itemindex);
+            res.send({"header":"data", "data":data});
         }
         else if (ajaxdata && ajaxdata.header == "removeunit") {
             console.log(ajaxdata.data);
-            success = function(user) {
-                res.send(ajaxdata.data);
-            };
+            res.send(ajaxdata.data);
         }
-        else //if no ajaxdata, send userdata
-            success = function(user) {
-                res.render("user", {userdata:JSON.stringify(user)});
-            };
-
-        success();
-    });
-    
-    app.get("/equip", function(req, res) {
-        var query = req.query;
-        var index = query.unitindex;
         
-        var ajaxdata = (query.jsonData) ? JSON.parse(query.jsonData) : 0;
-        var success;
-        if (ajaxdata && ajaxdata.header == "additem") {
-            success = function(user) {
-                var unit = (user) ? user.party.unitlist[index] : null;
-                app.game.create_item(unit, function(newitem) {
-                    if (newitem) res.send({"header":"newitem", "data":newitem});
-                    else res.send({"header":"error", "data":"Cannot add more items."});
-                }); 
-            };
-        }
-        else if (ajaxdata && ajaxdata.header == "getitems") {
-            success = function(user) {
-                var unit = (user) ? user.party.unitlist[index] : null;
-                res.send({"header":"itemlist", "data":unit.equip});
-            };
+        //items
+        else if (ajaxdata && ajaxdata.header == "additem") {
+            app.game.create_item(unit, function(newitem) {
+                if (newitem) res.send({"header":"newitem", "data":newitem});
+                else res.send({"header":"error", "data":"Cannot add more items."});
+            }); 
         }
         else if (ajaxdata && ajaxdata.header == "dropitem") {
-            success = function(user) {
-                app.game.get_party(user, function(party) {
-                    res.send(party);
-                });
-            };
+            app.game.get_party(req.user, function(party) {
+                res.send(party);
+            });
         }
         else if (ajaxdata && ajaxdata.header == "removeitem") {
             console.log(ajaxdata.data);
-            success = function(user) {
-                res.send(ajaxdata.data);
-            };
+            res.send(ajaxdata.data);
         }
-        else //if no ajaxdata, send unitdata and unitindex
-            success = function(user) {
-                var unit = (user) ? user.party.unitlist[index] : null;
-                res.render("equip", {unitdata: JSON.stringify(unit), unitindex: index});
-            };
-        
-        success();
+        else
+            res.send({"header":"error", "data": "FUCK!"});
     });
 };
